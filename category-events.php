a62efdd49a213b9a521f54138abc36c0b15518a2
Charlotte Dann <ask@pouretrebelle.com>
2015-12-30 15:42:26 +0000

    do up events page

--- category-events.php
+++ category-events.php
@@ -1,3 +1,5 @@
+<?php /* Template Name: Events */ ?>
+
 <?php get_header(); ?>
 
 <div class="container">
@@ -6,57 +8,56 @@
 
 		<main id="content" class="full-width" role="main" itemprop="mainContentOfPage" itemscope="itemscope" itemtype="http://schema.org/Blog">
 
-			<div class="clearfix">
+		<div class="clearfix category-events">
 
-			<?php
-			/*
-				if ( get_query_var('paged') ) {
-					$paged = get_query_var('paged');
-				} else if ( get_query_var('page') ) {
-					$paged = get_query_var('page');
-				} else {
-					$paged = 1;
-				}
 
-				$args = array( 'post_type' => 'portfolio', 'posts_per_page' => 9, 'paged' => $paged );
+        <?php 
+            $args = array(
+                'category_name'  => 'event',
+                'posts_per_page' => 12,
+                'post_status'    => 'future,publish',
+            );
 
-				$temp = $wp_query;
-				$wp_query = null;
-				$wp_query = new WP_Query();
-				$wp_query->query( $args );
+            $events = new WP_Query($args);
+        ?>
 
-				$count = 1;
-				*/
-			?>
 
-		
-
-		<div class="clearfix">
-
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<?php if ( $events->have_posts() ) : ?>
+			<?php while ( $events->have_posts() ) : $events->the_post(); ?>
 
 				<!-- Article -->
-				<article class="post<?php if( $count %3 == 0 ) { echo ' last'; }; ?>" itemscope="itemscope" itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
-					<header class="entry-header" itemscope="itemscope" itemtype="http://schema.org/WPHeader">
+				<article class="clearfix post<?php if( $count %3 == 0 ) { echo ' last'; }; ?>" itemscope="itemscope" itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
 
-						<?php if ( has_post_thumbnail() ) : ?>
-							<div class="item">
-								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-									<div class="contentlist_thumbnail"><?php the_post_thumbnail( 's' ); ?></div>
-									<div class="overlay"></div>
-									<span class="view"><?php _e( 'Watch', 'mighty' ); ?></span>
-								</a>
-							</div>
-						<?php endif; ?>
+					<?php if ( has_post_thumbnail() ) : ?>
+						<div class="item entry-thumb">
+							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+								<?php the_post_thumbnail( 's' ); ?>
+							</a>
+						</div>
+					<?php endif; ?>
 
-						<h2 class="entry-title" itemprop="headline">
-							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a>
-						</h2>
+					<div class="entry-wrap">
 
-					</header>
+						<header class="entry-header" itemscope="itemscope" itemtype="http://schema.org/WPHeader">
 
-					<div class="entry-content" itemprop="text">
-						<?php the_excerpt(); ?>
+							<h2 class="entry-title" itemprop="headline">
+								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a>
+							</h2>
+
+							<?php
+							$field_date = get_field('date');
+							if ($field_date) :
+								$date = DateTime::createFromFormat('Ymd', $field_date);
+							?>
+								<h5 class="entry-date"><?php echo $date->format('F d, Y'); ?></h5>
+							<?php endif; ?>
+
+						</header>
+
+						<div class="entry-content" itemprop="text">
+							<?php the_excerpt(); ?>
+						</div>
+
 					</div>
 
 				</article>
